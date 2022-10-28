c2bf53010801b577ae1bfd79ff9d1254fa173a53
nembo93 <francesco.c193@libero.it>
2022-10-28 10:48:16 +0200

    finiyo

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ const numeri = [
 ]
 
 let somDisp= 0;
-console.log(numeri.length)
+
 let i = 0;
 while (i < numeri.length) {
-    console.log(numeri[i]);
+    
     if (i % 2 !== 0){
         let disp = numeri[i];
-        let somDisp = somDisp + disp;
+        somDisp = somDisp + disp;
     }
     i++;
 }
