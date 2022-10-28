f5b78b8007720b7c15d967cd4755e012b06a052b
nembo93 <francesco.c193@libero.it>
2022-10-28 10:39:27 +0200

    modifica

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ const numeri = [
 ]
 somDisp= 0;
 
+
 while (let i = 0; i < numeri.length) {
     console.log(numeri[i]);
     if (i % 2 !== 0){
         let somDisp = somDisp + numeri[i];
     }
 }
+console.log(somDisp);
