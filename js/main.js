fc5b5edd7fd093fd8b752b071d8a79703fdb1167
nembo93 <francesco.c193@libero.it>
2022-10-28 10:38:07 +0200

    snack5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const numeri = [
-    `1`,
-    `2`,
-    `3`,
-    `4`,
-    `5`
+    1,
+    2,
+    3,
+    4,
+    5
 ]
-
+somDisp= 0;
 
 while (let i = 0; i < numeri.length) {
     console.log(numeri[i]);
+    if (i % 2 !== 0){
+        let somDisp = somDisp + numeri[i];
+    }
 }
